90719bde0898bff19fef65577294be2a2fe69c24
AJBaliga28 <ajbaliga28@pop-os.localdomain>
2024-10-08 13:12:14 +0530

    Some additions for the API issue.

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // Bearer <token>
+  const authHeader = req.headers.authorization;
+  console.log("Authorization Header:", authHeader); // Add this line
 
+  const token = authHeader?.split(" ")[1]; // Bearer <token>
   if (!token) {
     return res.status(401).json({ message: "Authorization token is missing" });
   }
